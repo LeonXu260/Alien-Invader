0688e9ebd88cb8ba176a79db4aaadd5d2e272c44
Leon Xu <19643979+LeonXu260@users.noreply.github.com>
2020-04-05 20:42:45 -0700

    Add files via upload

--- game_stats.py
+++ game_stats.py
@@ -1,19 +1,19 @@
-class GameStats():
-    """跟踪游戏的统计信息"""
-
-    def __init__(self, ai_settings):
-        """初始化统计信息"""
-        self.ai_settings = ai_settings
+class GameStats:
+    """Track statistics for Alien Invasion."""
+    
+    def __init__(self, ai_game):
+        """Initialize statistics."""
+        self.settings = ai_game.settings
         self.reset_stats()
 
-        # 让游戏一开始处于非活动状态
+        # Start game in an inactive state.
         self.game_active = False
 
-        # 在任何情况下都不应重置最高得分
+        # High score should never be reset.
         self.high_score = 0
-        self.level = 1
-
+        
     def reset_stats(self):
-        """初始化在游戏运行期间可能变化的统计信息"""
-        self.ship_left = self.ai_settings.ship_limit
+        """Initialize statistics that can change during the game."""
+        self.ships_left = self.settings.ship_limit
         self.score = 0
+        self.level = 1
--- settings.py
+++ settings.py
@@ -1,49 +1,48 @@
-class Settings():
-    """存储《外星人入侵》的所有设置的类"""
+class Settings:
+    """A class to store all settings for Alien Invasion."""
 
     def __init__(self):
-        """初始化游戏的设置"""
-        # 屏幕设置
-        self.screen_width = 800
-        self.screen_height = 600
+        """Initialize the game's static settings."""
+        # Screen settings
+        self.screen_width = 1200
+        self.screen_height = 800
         self.bg_color = (230, 230, 230)
 
-        # 飞船的设置
+        # Ship settings
         self.ship_limit = 3
 
-        # 子弹设置
+        # Bullet settings
         self.bullet_width = 3
         self.bullet_height = 15
-        self.bullet_color = 60, 60, 60
+        self.bullet_color = (60, 60, 60)
         self.bullets_allowed = 3
 
-        # 外星人设置
+        # Alien settings
         self.fleet_drop_speed = 10
-       
 
-        # 加快游戏节奏的速度
+        # How quickly the game speeds up
         self.speedup_scale = 1.1
-        # 外星人点数的提高速度
+        # How quickly the alien point values increase
         self.score_scale = 1.5
-        
+
         self.initialize_dynamic_settings()
 
     def initialize_dynamic_settings(self):
-        """初始化随游戏进行而变化的设置"""
-        self.ship_speed_factor = 1.5
-        self.bullet_speed_factor = 3
-        self.alien_speed_factor = 1
-        # fleet_direction为1表示向右移，为-1表示向左移
+        """Initialize settings that change throughout the game."""
+        self.ship_speed = 1.5
+        self.bullet_speed = 3.0
+        self.alien_speed = 1.0
+
+        # fleet_direction of 1 represents right; -1 represents left.
         self.fleet_direction = 1
 
-        # 记分
+        # Scoring
         self.alien_points = 50
 
     def increase_speed(self):
-        """提高速度设置和外星人点数"""
-        self.ship_speed_factor *= self.speedup_scale
-        self.bullet_speed_factor *= self.speedup_scale
-        self.alien_speed_factor *= self.speedup_scale
+        """Increase speed settings and alien point values."""
+        self.ship_speed *= self.speedup_scale
+        self.bullet_speed *= self.speedup_scale
+        self.alien_speed *= self.speedup_scale
 
         self.alien_points = int(self.alien_points * self.score_scale)
-        print(self.alien_points)
